11045b58dcc734eff4c323172169254c84a09d3c
unknown <runjankumar786@gmail.com>
2024-03-20 05:28:14 +0530

    first episode 1 complete

---
+++ app.js
@@ -0,0 +1,12 @@
+const heading = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", {}, "i am a h1 tag"),
+    React.createElement("h2", {}, "i am a h2 tag"),
+  ]),
+  React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", {}, "i am a h1 tag"),
+    React.createElement("h2", {}, "i am a h2 tag"),
+  ]),
+]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
